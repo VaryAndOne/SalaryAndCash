31c5e3f897c63eb8f65fbb3909710bf5c7c9d900
vary <VaryAndOne@163.com>
2017-05-29 16:41:39 +0800

    change3

--- app/src/main/java/com/vary/salaryandcash/modules/MainActivity.java
+++ app/src/main/java/com/vary/salaryandcash/modules/MainActivity.java
@@ -8,62 +8,24 @@ import android.widget.TextView;
 
 import com.vary.salaryandcash.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
-    private Button protocol,format,codec,filter;
-    private TextView tv_info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
     }
 
-    private void init() {
-        protocol = (Button) findViewById(R.id.btn_protocol);
-        format = (Button) findViewById(R.id.btn_format);
-        codec = (Button) findViewById(R.id.btn_codec);
-        filter = (Button) findViewById(R.id.btn_filter);
-        tv_info = (TextView) findViewById(R.id.tv_info);
-
-        protocol.setOnClickListener(this);
-        format.setOnClickListener(this);
-        codec.setOnClickListener(this);
-        filter.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_protocol:
-                tv_info.setText(urlprotocolinfo());
-                break;
-            case R.id.btn_format:
-                tv_info.setText(avformatinfo());
-                break;
-            case R.id.btn_codec:
-                tv_info.setText(avcodecinfo());
-                break;
-            case R.id.btn_filter:
-                tv_info.setText(avfilterinfo());
-                break;
-            default:
-                break;
-        }
-    }
 
     public native String stringFromJNI();
-
     public native String urlprotocolinfo();
     public native String avformatinfo();
     public native String avcodecinfo();
     public native String avfilterinfo();
-
-
 }

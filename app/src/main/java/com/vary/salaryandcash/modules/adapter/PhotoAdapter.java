baff4ced73044557b9615cfdd962e0891a3974ce
vary <VaryAndOne@163.com>
2017-06-08 16:54:30 +0800

    imageUtils

--- app/src/main/java/com/vary/salaryandcash/modules/adapter/PhotoAdapter.java
+++ app/src/main/java/com/vary/salaryandcash/modules/adapter/PhotoAdapter.java
@@ -1,19 +1,27 @@
 package com.vary.salaryandcash.modules.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
 import com.vary.salaryandcash.R;
 import com.vary.salaryandcash.base.BaseRecyclerAdapter;
 import com.vary.salaryandcash.base.CommonHolder;
 import com.vary.salaryandcash.mvp.model.Salary;
+import com.vary.salaryandcash.utilities.ImageUtils;
 
 import butterknife.Bind;
+
+import static com.vary.salaryandcash.utilities.ImageUtils.zoomImg;
 
 /**
  * Created by lcodecore on 2016/12/7.
@@ -28,7 +36,6 @@ public class PhotoAdapter extends BaseRecyclerAdapter<Salary> {
     }
 
     class CardHolder extends CommonHolder<Salary> {
-
         @Bind(R.id.tv_info)
         TextView tv_info;
 
@@ -41,11 +48,32 @@ public class PhotoAdapter extends BaseRecyclerAdapter<Salary> {
 
         @Override
         public void bindData(Salary photo) {
-//            iv_pic.setImageResource(photo.imgSrc);
-//            tv_info.setText(photo.name);
-            Glide.with(itemView.getContext()).load(photo.getMicroVideo())
-                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                    .into(iv_pic);
+            Glide.with(itemView.getContext())
+                    .load(photo.getMicroVideo())
+                    .asBitmap()
+                    .into(new SimpleTarget<Bitmap>(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL) {
+
+
+                        @Override
+                        public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
+
+
+                            //这个bitmap就是你图片url加载得到的结果
+                            //获取bitmap信息，可赋值给外部变量操作，也可在此时行操作。
+                            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) iv_pic.getLayoutParams();//获取你要填充图片的布局的layoutParam
+//                            layoutParams.height = (int) (((float) bitmap.getHeight()) / bitmap.getWidth() * ImageUtils.getScreenWidth(itemView.getContext()) / 2 );
+                            layoutParams.height = (int) (((float) bitmap.getHeight()) / bitmap.getWidth() * ImageUtils.getScreenWidth(itemView.getContext())/1.2);
+                            //因为是2列,所以宽度是屏幕的一半,高度是根据bitmap的高/宽*屏幕宽的一半
+                            layoutParams.width =  ImageUtils.getScreenWidth(itemView.getContext()) / 2;//这个是布局的宽度
+                            iv_pic.setLayoutParams(layoutParams);//容器的宽高设置好了
+                            bitmap = zoomImg(bitmap, layoutParams.width, layoutParams.height);
+                            // 然后在改变一下bitmap的宽高
+                            iv_pic.setImageBitmap(bitmap);
+                        }
+
+
+                    });
+
             iv_pic.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -57,7 +85,6 @@ public class PhotoAdapter extends BaseRecyclerAdapter<Salary> {
                 }
             });
         }
-
 
     }
 
---
+++ app/src/main/java/com/vary/salaryandcash/utilities/ImageUtils.java
@@ -0,0 +1,38 @@
+package com.vary.salaryandcash.utilities;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Created by Administrator on 2017-06-08.
+ */
+
+public class ImageUtils {
+    //改变bitmap尺寸的方法
+    public static Bitmap zoomImg(Bitmap bm, int newWidth, int newHeight) {
+        // 获得图片的宽高
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        // 计算缩放比例
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // 取得想要缩放的matrix参数
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        // 得到新的图片
+        Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+        return newbm;
+    }
+    //获取屏幕宽度的方法
+    public static int getScreenWidth(Context context)
+    {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+}

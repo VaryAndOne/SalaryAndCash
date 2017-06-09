9fbe007ba493f306534f378e0b5e7a926246be39
vary <VaryAndOne@163.com>
2017-06-09 18:37:06 +0800

    app_simple5

--- app/src/main/java/com/vary/salaryandcash/modules/MainActivity.java
+++ app/src/main/java/com/vary/salaryandcash/modules/MainActivity.java
@@ -3,35 +3,23 @@ package com.vary.salaryandcash.modules;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.vary.salaryandcash.R;
 import com.vary.salaryandcash.modules.adapter.MyPagerAdapter;
+import com.vary.salaryandcash.modules.fragment.HomeFragment;
+import com.vary.salaryandcash.modules.fragment.MainFragment;
 
-public class MainActivity extends AppCompatActivity  {
+import me.yokeyword.fragmentation.SupportActivity;
+
+public class MainActivity extends SupportActivity {
     private ViewPager mPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-        findViewById(R.id.account).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PersonActivity.class);
-                startActivity(intent);
-            }
-        });
-        findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                startActivity(intent);
-            }
-        });
+        if (savedInstanceState == null) {
+            loadRootFragment(R.id.frame_Layout_root, new MainFragment());
+        }
     }
 }
---
+++ app/src/main/java/com/vary/salaryandcash/modules/fragment/MainFragment.java
@@ -0,0 +1,54 @@
+package com.vary.salaryandcash.modules.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.vary.salaryandcash.R;
+import com.vary.salaryandcash.modules.CameraActivity;
+import com.vary.salaryandcash.modules.adapter.MyPagerAdapter;
+
+import me.yokeyword.fragmentation.SupportFragment;
+
+/**
+ * Created by Administrator on 2017-06-09.
+ */
+
+public class MainFragment extends SupportFragment {
+    private View view;
+    private ViewPager mPager;
+    @Override
+    public void onSupportVisible() {
+        super.onSupportVisible();
+        // todo,当该Fragment对用户可见时
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        mPager = (ViewPager) view.findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getActivity().getSupportFragmentManager()));
+        view.findViewById(R.id.account).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), PersonActivity.class);
+//                startActivity(intent);
+                start(new HomeFragment());
+            }
+        });
+        view.findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+}

f0df70331735bcd16cb8322870224da54b9748ff
vary <VaryAndOne@163.com>
2017-06-11 12:02:05 +0800

    change app1

--- app/src/main/java/com/vary/salaryandcash/modules/MainActivity.java
+++ app/src/main/java/com/vary/salaryandcash/modules/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends SupportActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         splashFragment = new SplashFragment();
         final ViewStub mainLayout = (ViewStub) findViewById(R.id.content_viewstub);
